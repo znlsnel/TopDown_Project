ab46ed8cac51214e6761695b7806625ad4254793
Udi <dnel0605@gmail.com>
2025-02-28 23:12:50 +0900

    tileMap

--- Assets/Scripts/Entity/PlayerController.cs
+++ Assets/Scripts/Entity/PlayerController.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : BaseController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private Camera camera;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	protected override void Start()
+	{
+		base.Start();
+		camera = Camera.main;
+	}
+
+	protected override void handleAction()
+	{
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
+		movementDirection = new Vector2(horizontal, vertical).normalized;
+
+		Vector2 mousePosition = Input.mousePosition;
+		Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+		lookDirection = (worldPos - (Vector2)transform.position);
+
+		if (lookDirection.magnitude < .9f)
+			lookDirection = Vector2.zero;
+		else
+			lookDirection = lookDirection.normalized;
+	}
+	
+
 }
