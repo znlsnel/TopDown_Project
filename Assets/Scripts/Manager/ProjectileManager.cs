84ba744c57aeb87337aa9565baf13069340e8b72
Udi <dnel0605@gmail.com>
2025-03-02 00:37:13 +0900

    [feat] projectile

--- Assets/Scripts/Manager/ProjectileManager.cs
+++ Assets/Scripts/Manager/ProjectileManager.cs
@@ -2,15 +2,22 @@ using UnityEngine;
 
 public class ProjectileManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	private static ProjectileManager instance;
+	public static ProjectileManager Instance { get { return instance; } }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField] private GameObject[] projectilePrefabs;
+
+	private void Awake()
+	{
+		instance = this;
+	}
+	public void ShootBullet(RangeWeaponHandler rangeWeaponHandler, Vector2 startPostiion, Vector2 direction)
+	{
+		GameObject origin = projectilePrefabs[rangeWeaponHandler.BulletIndex];
+		GameObject obj = Instantiate(origin, startPostiion, Quaternion.identity);
+
+		ProjectileController projectileController = obj.GetComponent<ProjectileController>();
+		projectileController.Init(direction, rangeWeaponHandler);
+	}
+
 }
---
+++ Assets/Scripts/Weapon/ProjectileController.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+	[SerializeField] private LayerMask levelCollisionLayer;
+
+	private RangeWeaponHandler rangeWeaponHandler;
+
+	private float currentDuration;
+	private Vector2 direction;
+	private bool isReady;
+	private Transform pivot;
+
+	private Rigidbody2D _rigidbody;
+	private SpriteRenderer spriteRenderer;
+
+	public bool fxOnDestory = true;
+
+	private void Awake()
+	{
+		spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+		_rigidbody = GetComponent<Rigidbody2D>();
+		pivot = transform.GetChild(0);
+	}
+
+	private void Update()
+	{
+		if (!isReady)
+			return;
+
+		currentDuration += Time.deltaTime;
+
+		if (currentDuration > rangeWeaponHandler.Duration)
+			DestroyProjectile(transform.position, false);
+
+		_rigidbody.linearVelocity = direction * rangeWeaponHandler.Speed;
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if ((levelCollisionLayer.value == (levelCollisionLayer.value | 1 << collision.gameObject.layer)))
+			DestroyProjectile(collision.ClosestPoint(transform.position) - direction * .2f, fxOnDestory);
+
+		else if (rangeWeaponHandler.target.value == (rangeWeaponHandler.target.value | (1 << collision.gameObject.layer)))
+			DestroyProjectile(collision.ClosestPoint(transform.position), fxOnDestory);
+
+	}
+
+	public void Init(Vector2 direction, RangeWeaponHandler weaponHandler)
+	{
+		rangeWeaponHandler = weaponHandler;
+
+		this.direction = direction;
+		currentDuration = 0;
+		transform.localScale = Vector3.one * weaponHandler.BulletSize;
+		spriteRenderer.color = weaponHandler.ProjectileColor;
+
+		transform.right = this.direction;
+		 
+		if (this.direction.x < 0)
+			pivot.localRotation = Quaternion.Euler(180, 0, 0);
+		else
+			pivot.localRotation = Quaternion.Euler(0, 0, 0);
+
+		isReady = true;
+	}
+
+	private void DestroyProjectile(Vector3 position, bool createFx)
+	{
+		Destroy(this.gameObject);
+	}
+}
