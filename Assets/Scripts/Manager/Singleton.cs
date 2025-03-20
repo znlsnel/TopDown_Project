6b544084aec2b6776fa93f9609a82f5d56446050
Udi <dnel0605@gmail.com>
2025-03-20 15:24:52 +0900

    [feat] Camera Bounding Shape

---
+++ Assets/Scripts/Manager/Singleton.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    private static readonly object lockObject = new object();
+    private static bool applicationIsQuitting = false;
+
+    public static T Instance
+    {
+        get
+        {
+            if (applicationIsQuitting)
+            {
+                Debug.LogWarning($"[Singleton] Instance '{typeof(T)}' already destroyed on application quit. Won't create again - returning null.");
+                return null;
+            }
+
+            lock (lockObject)
+            {
+                if (instance == null)
+                {
+                    instance = (T)FindObjectOfType(typeof(T));
+
+                    if (instance == null)
+                    {
+                        GameObject singletonObject = new GameObject();
+                        instance = singletonObject.AddComponent<T>();
+                        singletonObject.name = typeof(T).ToString();
+
+                        DontDestroyOnLoad(singletonObject);
+                    }
+                }
+
+                return instance;
+            }
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        applicationIsQuitting = true;
+    }
+
+    protected virtual void OnApplicationQuit()
+    {
+        applicationIsQuitting = true;
+    }
+}
